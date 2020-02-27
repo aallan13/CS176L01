1ae9e6a70d7b408fe13956d69ed04d47f9f63934
aallan13 <55116506+aallan13@users.noreply.github.com>
2020-02-26 19:53:20 -0500

    Update VenueTester.java

--- VenueProject/VenueTester.java
+++ VenueProject/VenueTester.java
@@ -9,16 +9,16 @@ public class VenueTester {
 		Venue prudentialCenter = new Venue(4, 5, 10);
 
 		for(int i = 0; i < 100; i++) {
-			int a = generator.nextInt(4);
-			int b = generator.nextInt(5);
-			int c = generator.nextInt(10);
+			int num1 = generator.nextInt(4);
+			int num2 = generator.nextInt(5);
+			int num3 = generator.nextInt(10);
 
 
 			if(prudentialCenter.seatLookup(a, b, c) == true) {
-				System.out.println("Section: " + a +", Row: "+ b +", Seat: "+ c + " is taken");
+				System.out.println("Section: " + num1 +", Row: "+ num2 +", Seat: "+ num3 + " is taken");
 			}
 			else {
-				System.out.println("Section: " + a +", Row: "+ b +", Seat: "+ c + " is awarded");
+				System.out.println("Section: " + num1 +", Row: "+ num2 +", Seat: "+ num3 + " is awarded");
 			}
 
 
